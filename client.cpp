4a035e8174a12ddb04c63fa348e16568e8d171eb
wachirawit wacharak <tauhoo_ice@hotmail.com>
2019-08-29 20:29:01 +0700

    add loop test in client

--- client.cpp
+++ client.cpp
@@ -2,6 +2,9 @@
 ** client.c -- a stream socket client demo
 */
 
+#include <pthread.h>
+#include <iostream>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,30 +17,47 @@
 
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
+#define PORT "3490" // the port client will be connecting to
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+using namespace std;
+
+float sumtime = 0;
+pthread_mutex_t lock;
+
+typedef struct thread_arg
+{
+    int argc;
+    char **argv;
+} thread_arg;
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+void *sendRequest(void *arg) //int argc, char *argv[])
 {
-    int sockfd, numbytes;  
+    thread_arg *argument = (thread_arg *)arg;
+    int argc = argument->argc;
+    char **argv = argument->argv;
+
+    int sockfd, numbytes;
     char buf[MAXDATASIZE];
     struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: client hostname\n");
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: client hostname\n");
         exit(1);
     }
 
@@ -45,20 +65,25 @@ int main(int argc, char *argv[])
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+        return;
     }
-
+    clock_t t1, t2;
+    t1 = clock();
     // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+                             p->ai_protocol)) == -1)
+        {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("client: connect");
             continue;
@@ -67,27 +92,58 @@ int main(int argc, char *argv[])
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "client: failed to connect\n");
-        return 2;
+        return;
     }
 
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
-    printf("client: connecting to %s\n", s);
+              s, sizeof s);
 
     freeaddrinfo(servinfo); // all done with this structure
 
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1)
+    {
         perror("recv");
         exit(1);
     }
+    t2 = clock();
+    float diff((float)t2 - (float)t1);
+    float seconds = diff / CLOCKS_PER_SEC;
+
+    pthread_mutex_lock(&lock);
+    sumtime += seconds;
+    pthread_mutex_unlock(&lock);
 
     buf[numbytes] = '\0';
 
-    printf("client: received '%s'\n",buf);
-
     close(sockfd);
+}
+
+int main(int argc, char *argv[])
+{
+
+    cout << "respondtime" << endl;
+
+    // Call the function, here sort()
+    for (int o = 165; o <= 1000; o++)
+    {
+        pthread_t th[o];
+        sumtime = 0;
+        for (int i = 0; i < o; i++)
+        {
+            thread_arg *arg = new thread_arg();
+            arg->argc = argc;
+            arg->argv = argv;
+            pthread_create(&th[i], NULL, sendRequest, arg);
+        }
+        for (int p = 0; p < o; o++)
+        {
+            pthread_join(th[p], NULL);
+        }
+        cout << sumtime / o << endl;
+    }
 
     return 0;
 }
